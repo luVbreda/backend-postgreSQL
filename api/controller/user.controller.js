a4e5f643b5a8df78687e1e0e6544fbabf792874f
Lucas Breda <mascarenhas-lblm@hotmail.com>
2025-05-06 19:11:16 -0300

    Update 06/05/2025

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -31,16 +31,17 @@ const login = async (req, res) => {
 
     try {
         const user = await db.users.findOne({ where: { email } });
+        console.log("User found:", user); // Adicione este log para depuração
         if (!user) {
             return res.status(404).json({ message: 'User not found' });
         }
 
-        const isMatch = await bcrypt.compare(password, db.users.password);
+        const isMatch = await bcrypt.compare(password, user.password); // Corrigido aqui
         if (!isMatch) {
             return res.status(401).json({ message: 'Invalid credentials' });
         }
 
-        const token = jwt.sign({ id: db.users._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
         return res.status(200).json({ message: 'Login successful', token });
     } catch (error) {
